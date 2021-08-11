358ee0054e64e0d50be855f50710fd4e9fac862a
Laminatik <59961520+Laminatik@users.noreply.github.com>
2021-08-11 10:36:47 +0200

    Homework01

---
+++ Homework/task3/Fruit.java
@@ -0,0 +1,8 @@
+package Homework.task3;
+
+public interface Fruit {
+
+    String getName();
+
+    double getWeight();
+}
---
+++ Homework/task3/Orange.java
@@ -0,0 +1,13 @@
+package Homework.task3;
+
+public class Orange implements Fruit {
+    @Override
+    public String getName() {
+        return "Orange";
+    }
+
+    @Override
+    public double getWeight() {
+        return 1.5;
+    }
+}
